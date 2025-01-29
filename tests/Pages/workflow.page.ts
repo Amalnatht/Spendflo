76365f7c07dcdee05ca08ebf8d5fc2c219fe7627
Amalnatht <amalnath@spendflo.com>
2025-01-29 10:30:27 +0530

    minor changes to wf

--- tests/Pages/workflow.page.ts
+++ tests/Pages/workflow.page.ts
@@ -1,8 +1,10 @@
 import { Page, Locator, expect, Response } from '@playwright/test';
-import {generateRandomText , draganddrop } from "../utils/environment.utils";
+import {generateRandomText , draganddrop } from "../utils/general.utils";
+import { promises } from 'dns';
+import { StringLiteral } from 'typescript';
 
 
-export class LoginPage {
+export class WorkflowPageAndStudio {
     private page: Page;
     private settingsButton: Locator;
     private wfNavigationSideBar: Locator;
@@ -12,6 +14,7 @@ export class LoginPage {
     private createWorkflowButton: Locator;
     private workflowStudioCloseButton: Locator;
     private intakeform3dots: Locator;
+    private postIntakeForm3dots: Locator;
     private taskEditButton: Locator;
     private saveAndContinueButtonInTaskform : Locator;
     private addSectionButton : Locator;
@@ -59,7 +62,8 @@ export class LoginPage {
     private formResponseRequiredToggle : Locator;
     private formSelectTypeofAttachment : Locator;
     private formattachemtquestionFirstoptionselection : Locator;
-    private formUserQuestionUserTypeSelect : Locator;
+    private formUserQuestionUserTypeSelectAdmin : Locator;
+    private bottomNodePlusButton : Locator;
 
     constructor(page: Page) {
         this.page = page;
@@ -118,18 +122,32 @@ export class LoginPage {
         this.formResponseRequiredToggle = page.locator("//label[@for='Response required']//following-sibling::div//button");
         this.formSelectTypeofAttachment = page.locator("//label[@for='Select the type of attachment']/following-sibling::div//button");
         this.formattachemtquestionFirstoptionselection = page.locator("//div[@role='option'][1]");
-        this.formUserQuestionUserTypeSelect = page.locator("//label[text()='Users with role: Admin']");
+        this.formUserQuestionUserTypeSelectAdmin = page.locator("//label[text()='Users with role: Admin']");
+        this.bottomNodePlusButton = page.locator("//button[contains(@class, 'absolute bottom')]");
+        this.postIntakeForm3dots = page.locator('//span[text()="Post Intake"]/ancestor::div[2]//button')
 
 
     }
 
-    async createANewWorkflow(){
+    async createANewWorkflow():Promise<string>{
         await this.addWorkflowButton.click();
         await this.createaNewWorkflowButton.click();
         let workflowName = generateRandomText(5);
         await this.wfNameInputField.fill(workflowName);
         await this.createWorkflowButton.click();
         await this.workflowStudioCloseButton.click();
+        return workflowName;
+    }
+
+    async createANewPhase(){
+        await this.completedPhase.hover();
+        await this.plusIconNextToCompletedPhase.click();
+        await this.phaseNameInputField.fill(generateRandomText(5));
+        await this.createPhaseButton.click();
+    }
+
+    async createANewTaskfromEmptyPhase(){
+        await this.plusAddTaskButton.click();
     }
 
     async intakeOverviewContinueWithDefaultValues(){
@@ -138,18 +156,35 @@ export class LoginPage {
         await this.saveAndContinueButtonInTaskform.click();
     }
     
-    async formSectionCompletion(){
+    async postIntakeTaskedit(){
+        await this.postIntakeForm3dots.click();
+        await this.taskEditButton.click(); 
+    }
+
+    async formSectionCompletionWithVendorQuestionAndOtherQuestionTypes():Promise<string>{
         await this.addSectionButton.click();
         await this.page.keyboard.press('Enter');
         let source : Locator; let target: Locator;
+        source = this.vendorQuestionBlock;
+        target = this.section1Block;
+        await draganddrop(source,target,this.page);
+        await this.saveButtonForms.click();
+        await this.page.waitForTimeout(3000);
+        return await this.formSectionCompletion();
+        
+    }
+
+    async createSectionInFormsWithDefaultName(){
+        await this.addSectionButton.click();
+        await this.page.keyboard.press('Enter');
+    }
+
+    async formSectionCompletion():Promise<string>{
+        let source : Locator; let target: Locator;
         let randomNumber = Math.floor(Math.random() * 10) + 1;
-        if(randomNumber==1){
-            source = this.vendorQuestionBlock;
-            target = this.section1Block;
-            await draganddrop(source,target,this.page);
-        }
-        else if (randomNumber==2){
+        if (randomNumber==1){
             source = this.shortTextQuestionBlock;
+            await source.scrollIntoViewIfNeeded();
             target = this.section1Block;
             await draganddrop(source,target,this.page);
             const questionpassed = "Short Text Question";
@@ -159,9 +194,12 @@ export class LoginPage {
             await this.saveButtonForms.click();
             await this.page.waitForTimeout(3000);
             await this.saveAndContinueButtonInTaskform.click();
+            return questionpassed;
+
         }
-        else if (randomNumber==3){
+        else if (randomNumber==2){
             source = this.longTextQuestionBlock;
+            await source.scrollIntoViewIfNeeded();
             target = this.section1Block;
             await draganddrop(source,target,this.page);
             const questionpassed = "Long Text Question";
@@ -171,9 +209,11 @@ export class LoginPage {
             await this.saveButtonForms.click();
             await this.page.waitForTimeout(3000);
             await this.saveAndContinueButtonInTaskform.click();
+            return questionpassed;
         }
-        else if (randomNumber==4){
+        else if (randomNumber==3){
             source = this.dropdownQuestionBlock;
+            await source.scrollIntoViewIfNeeded();
             target = this.section1Block;
             await draganddrop(source,target,this.page);
             const questionpassed = "Dropdown Question";
@@ -191,9 +231,11 @@ export class LoginPage {
             await this.saveButtonForms.click(); 
             await this.page.waitForTimeout(3000);
             await this.saveAndContinueButtonInTaskform.click();
+            return questionpassed;
         }
-        else if ( randomNumber == 5){
+        else if ( randomNumber == 4){
             source = this.singleChoiceQuestionBlock;
+            await source.scrollIntoViewIfNeeded();
             target = this.section1Block;
             await draganddrop(source,target,this.page);
             const questionpassed = "Single Choice Question";
@@ -211,9 +253,11 @@ export class LoginPage {
             await this.saveButtonForms.click(); 
             await this.page.waitForTimeout(3000);
             await this.saveAndContinueButtonInTaskform.click();
+            return questionpassed;
         }
-        else if(    randomNumber == 6){
+        else if(    randomNumber == 5){
             source = this.multipleChoiceQuestionBlock;
+            await source.scrollIntoViewIfNeeded();
             target = this.section1Block;
             await draganddrop(source,target,this.page);
             const questionpassed = "Multiple Choice Question";
@@ -231,9 +275,11 @@ export class LoginPage {
             await this.saveButtonForms.click(); 
             await this.page.waitForTimeout(3000);
             await this.saveAndContinueButtonInTaskform.click();
+            return questionpassed;
         }
-        else if(randomNumber == 7){
+        else if(randomNumber == 6){
             source = this.numberQuestionBlock;
+            await source.scrollIntoViewIfNeeded();
             target = this.section1Block;
             await draganddrop(source,target,this.page);
             const questionpassed = "Number Question";
@@ -244,10 +290,12 @@ export class LoginPage {
             await this.saveButtonForms.click(); 
             await this.page.waitForTimeout(3000);
             await this.saveAndContinueButtonInTaskform.click();
+            return questionpassed;
 
         }
-        else if(randomNumber == 8){
+        else if(randomNumber == 7){
             source = this.dateQuestionBlock;
+            await source.scrollIntoViewIfNeeded();
             target = this.section1Block;
             await draganddrop(source,target,this.page);
             const questionpassed = "Date Question";
@@ -258,9 +306,11 @@ export class LoginPage {
             await this.saveButtonForms.click(); 
             await this.page.waitForTimeout(3000);
             await this.saveAndContinueButtonInTaskform.click();
+            return questionpassed;
         }
-        else if(randomNumber == 9){
+        else if(randomNumber == 8){
             source = this.attachmentQuestionBlock;
+            await source.scrollIntoViewIfNeeded();
             target = this.section1Block;
             await draganddrop(source,target,this.page);
 
@@ -275,9 +325,11 @@ export class LoginPage {
             await this.saveButtonForms.click(); 
             await this.page.waitForTimeout(3000);
             await this.saveAndContinueButtonInTaskform.click();
+            return  questionpassed;
         }
-        else if(randomNumber == 10){
+        else if(randomNumber == 9){
             source = this.currencyQuestionBlock;
+            await source.scrollIntoViewIfNeeded();
             target = this.section1Block;
             await draganddrop(source,target,this.page);
             const questionpassed = "Currency Question";
@@ -288,9 +340,11 @@ export class LoginPage {
             await this.saveButtonForms.click(); 
             await this.page.waitForTimeout(3000);
             await this.saveAndContinueButtonInTaskform.click();
+            return questionpassed;
         }
-        else if(randomNumber == 11){
+        else if(randomNumber == 10){
             source = this.userselectQuestionBlock;
+            await source.scrollIntoViewIfNeeded();
             target = this.section1Block;
             await draganddrop(source,target,this.page);
             const questionpassed = "User select Question";
@@ -298,14 +352,16 @@ export class LoginPage {
             await this.formDescriptionField.fill("Description");
             await this.formResponseRequiredToggle.click();
 
-            await this.formattachemtquestionFirstoptionselection.click();
+            await this.formUserQuestionUserTypeSelectAdmin.click();
 
             await this.saveButtonForms.click(); 
             await this.page.waitForTimeout(3000);
             await this.saveAndContinueButtonInTaskform.click();
+            return questionpassed;
         }
         else{
-            throw new Error("Invalid Question Type");
+            console.log("Invalid Question Type");
+            return "Invalid question";
         }
     }
 
@@ -318,11 +374,9 @@ export class LoginPage {
         await this.page.waitForTimeout(4000);
         await this.saveAndContinueButtonInTaskform.click();
         await this.page.waitForTimeout(4000);
-
-
     }
 
-    async approvalCSMOverview(){
+    async approvalCSMOverview():Promise<string>{
         let taskname = generateRandomText(5);
         await this.taskNameInputField.fill(taskname);
         await this.approvalRadioButton.click();
@@ -331,10 +385,10 @@ export class LoginPage {
         await this.csmRoleFromdropdown.click();
         await this.page.waitForTimeout(3000);
         await this.saveAndContinueButtonInTaskform.click();
-        
+        return taskname;
     }
 
-    async completionCSMOverview(){
+    async completionCSMOverview():Promise<string>{
         let taskname = generateRandomText(5);
         await this.taskNameInputField.fill(taskname);
         await this.completionRadioButton.click();
@@ -343,10 +397,11 @@ export class LoginPage {
         await this.csmRoleFromdropdown.click();
         await this.page.waitForTimeout(3000);
         await this.saveAndContinueButtonInTaskform.click();
+        return taskname;
         
     }
 
-    async approvalEMOverview(){
+    async approvalEMOverview():Promise<string>{
         let taskname = generateRandomText(5);
         await this.taskNameInputField.fill(taskname);
         await this.approvalRadioButton.click();
@@ -355,10 +410,11 @@ export class LoginPage {
         await this.engagementManagerFromdropdown.click();
         await this.page.waitForTimeout(3000);
         await this.saveAndContinueButtonInTaskform.click();
+        return taskname;
         
     }
 
-    async completionEMOverview(){
+    async completionEMOverview():Promise<string>{
         let taskname = generateRandomText(5);
         await this.taskNameInputField.fill(taskname);
         await this.completionRadioButton.click();
@@ -367,10 +423,11 @@ export class LoginPage {
         await this.engagementManagerFromdropdown.click();
         await this.page.waitForTimeout(3000);
         await this.saveAndContinueButtonInTaskform.click();
+        return taskname;
         
     }
 
-    async approvalRequesterOverview(){
+    async approvalRequesterOverview():Promise<string>{
         let taskname = generateRandomText(5);
         await this.taskNameInputField.fill(taskname);
         await this.approvalRadioButton.click();
@@ -379,10 +436,11 @@ export class LoginPage {
         await this.requesterFromdropdown.click();
         await this.page.waitForTimeout(3000);
         await this.saveAndContinueButtonInTaskform.click();
+        return taskname;
         
     }
 
-    async completionRequesterOverview(){
+    async completionRequesterOverview():Promise<string>{
         let taskname = generateRandomText(5);
         await this.taskNameInputField.fill(taskname);
         await this.completionRadioButton.click();
@@ -391,10 +449,11 @@ export class LoginPage {
         await this.requesterFromdropdown.click();
         await this.page.waitForTimeout(3000);
         await this.saveAndContinueButtonInTaskform.click();
+        return taskname;
         
     }
 
-    async approvalRequesterteamownerOverview(){
+    async approvalRequesterteamownerOverview():Promise<string>{
         let taskname = generateRandomText(5);
         await this.taskNameInputField.fill(taskname);
         await this.approvalRadioButton.click();
@@ -403,10 +462,11 @@ export class LoginPage {
         await this.requesterTeamOwnerFromdropdown.click();
         await this.page.waitForTimeout(3000);
         await this.saveAndContinueButtonInTaskform.click();
+        return taskname;
         
     }
 
-    async completionRequesterteamownerOverview(){
+    async completionRequesterteamownerOverview():Promise<string>{
         let taskname = generateRandomText(5);
         await this.taskNameInputField.fill(taskname);
         await this.completionRadioButton.click();
@@ -415,10 +475,11 @@ export class LoginPage {
         await this.requesterTeamOwnerFromdropdown.click();
         await this.page.waitForTimeout(3000);
         await this.saveAndContinueButtonInTaskform.click();
+        return taskname;
         
     }
 
-    async approvalRequestermanagerOverview(){
+    async approvalRequestermanagerOverview():Promise<string>{
         let taskname = generateRandomText(5);
         await this.taskNameInputField.fill(taskname);
         await this.approvalRadioButton.click();
@@ -427,10 +488,11 @@ export class LoginPage {
         await this.requesterManagerFromdropdown.click();
         await this.page.waitForTimeout(3000);
         await this.saveAndContinueButtonInTaskform.click();
+        return taskname;
         
     }
 
-    async completionRequestermanagerOverview(){
+    async completionRequestermanagerOverview():Promise<string>{
         let taskname = generateRandomText(5);
         await this.taskNameInputField.fill(taskname);
         await this.completionRadioButton.click();
@@ -439,10 +501,11 @@ export class LoginPage {
         await this.requesterManagerFromdropdown.click();
         await this.page.waitForTimeout(3000);
         await this.saveAndContinueButtonInTaskform.click();
+        return taskname;
         
     }
 
-    async approvalSelectedUserOverview(){
+    async approvalSelectedUserOverview():Promise<string>{
         let taskname = generateRandomText(5);
         await this.taskNameInputField.fill(taskname);
         await this.approvalRadioButton.click();
@@ -452,10 +515,11 @@ export class LoginPage {
         await this.selectUserDropdownFirstOption.click();
         await this.page.waitForTimeout(3000);
         await this.saveAndContinueButtonInTaskform.click();
+        return taskname;
         
     }
 
-    async completionSelectedUserOverview(){
+    async completionSelectedUserOverview():Promise<string>{
         let taskname = generateRandomText(5);
         await this.taskNameInputField.fill(taskname);
         await this.completionRadioButton.click();
@@ -465,8 +529,73 @@ export class LoginPage {
         await this.selectUserDropdownFirstOption.click();
         await this.page.waitForTimeout(3000);
         await this.saveAndContinueButtonInTaskform.click();
-        
+        return taskname;
     }
 
+    async withoutform(){
+        await this.page.waitForTimeout(4000);
+        await this.saveAndContinueButtonInTaskform.click();
+        await this.page.waitForTimeout(4000);
 
+    }
+
+    async withoutChecklist(){
+        await this.page.waitForTimeout(4000);
+        await this.saveAndContinueButtonInTaskform.click();
+        await this.page.waitForTimeout(4000);
+        await this.saveAndContinueButtonInTaskform.click();
+        await this.page.waitForTimeout(4000);
+    }
+    
+    async hoverOverATaskAndClickonBottomnode(taskToBeHoveredOver : string){
+        let taskToHoverover = this.page.locator(`//span[text()='${taskToBeHoveredOver}']`);
+        await taskToHoverover.scrollIntoViewIfNeeded();
+        await taskToHoverover.hover();
+        await this.bottomNodePlusButton.click();
+
+    }
+    async hoverOverATaskAndClickonLeftnode(taskToBeHoveredOver : string){
+        let taskToHoverover = this.page.locator(`//span[text()='${taskToBeHoveredOver}']`);
+        await taskToHoverover.scrollIntoViewIfNeeded();
+        await taskToHoverover.hover();
+        await this.page.locator(`//span[text()='${taskToBeHoveredOver}']/ancestor::div[4]//div[@data-handlepos='left']`).click();
+        if(taskToBeHoveredOver!="Post Intake" && taskToBeHoveredOver!= "Intake Form"){
+            await this.addTaskButton.click();
+        }
+
+    }
+    async hoverOverATaskAndClickonRightnode(taskToBeHoveredOver : string){
+        let taskToHoverover = this.page.locator(`//span[text()='${taskToBeHoveredOver}']`);
+        await taskToHoverover.scrollIntoViewIfNeeded();
+        await taskToHoverover.hover();
+        await this.page.locator(`//span[text()='${taskToBeHoveredOver}']/ancestor::div[4]//div[@data-handlepos='right']`).click();
+        if(taskToBeHoveredOver!="Post Intake" && taskToBeHoveredOver!= "Intake Form"){
+            await this.addTaskButton.click();
+        }
+    }
+
+    async publishWorkflow(){
+        await this.publishButtoninWorkflowStudio.click();
+        await this.versionDescriptionField.fill("Version 1");
+        await this.page.waitForTimeout(2000);
+        await this.publishButtoninModal.click();
+        await this.page.waitForTimeout(12000);
+    }
+
+}
+
+export class Taskdetails {
+    private typeoftask : string;
+    private userrole : string;
+    private taskname : string;
+    private question : string;
+    private additonalQuestion : string
+    constructor( typeoftask : string, userrole : string, taskname : string , question : string, additonalQuestion: string = "empty"){ {
+        this.question = question;
+        this.typeoftask = typeoftask;
+        this.userrole = userrole;
+        this.taskname = taskname;
+        this.additonalQuestion = additonalQuestion;
+    }
+  }
 }
