7f69f3082df3a5e12756cf70a74d33e4ef6fed5c
Amalnatht <amalnath@spendflo.com>
2025-02-04 14:40:12 +0530

    changes to login steps file to accomodate new format

--- tests/Steps/Login/login.steps.ts
+++ tests/Steps/Login/login.steps.ts
@@ -3,6 +3,7 @@ import { chromium, Browser, Page, expect, Locator } from "@playwright/test";
 import { LoginPage } from "../../Pages/login.page";
 import * as usersData from "../../../Data/login.data.json";
 import { Users } from "../../../Data/login.data.interface";
+import { LoginUtils } from "../../utils/Loginutils/login.utils";
 
 setDefaultTimeout(60 * 5000);
 
@@ -12,33 +13,35 @@ let browser: Browser;
 let users: Users = usersData;
 
 let loginPage: LoginPage;
+let loginUtils : LoginUtils;
 
 Before(async function () {
   browser = await chromium.launch({ headless: false });
   const context = await browser.newContext();
   page = await context.newPage();
   loginPage = new LoginPage(page, "https://app.spendflo.com"); // Updated to include baseUrl
+  loginUtils = new  LoginUtils(loginPage);
 });
 
 // Updated: Combined step for navigation and login
 When("User navigates to Login page and logs in with credentials for user {string}", async function (user: string) {
   const userDetails = users[user];
-  await loginPage.navigateAndLogin(userDetails.email, userDetails.password);
+  await loginUtils.navigateAndLogin(userDetails.email, userDetails.password);
 });
 
 Then("User should be signed in", async function () {
-  await loginPage.loginsuccessful();
+  await loginUtils.loginsuccessful();
   // const ss = await loginPage.page.screenshot({ path: 'screenshot.png' });
 });
 
 When("Close Featurewalkthrough if exists", async function () {
-  await loginPage.checkforpendoGuideandClose();
+  await loginUtils.checkforpendoGuideandClose();
 });
 
 Then("Switch organization to {string} if user is superadmin", async function (orgname: string) {
-  let result = await loginPage.fetchTheOrgnamefromNavBar();
+  let result = await loginUtils.fetchTheOrgnamefromNavBar();
   if (result) {
-    await loginPage.switchToDesiredorg(orgname);
+    await loginUtils.switchToDesiredorg(orgname);
   } else {
     console.log("User logged in as non superadmin");
   }
